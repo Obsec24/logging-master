2d1baba5de427ad83c00a90ce124d236bc249b85
Obsec24 <obsecpolitecnico@gmail.com>
2025-12-09 11:41:53 -0500

    Cambio de versión de filebeat de 7.3.2  7.13.23

--- agent/install_agent.sh
+++ agent/install_agent.sh
@@ -3,7 +3,7 @@ then
 	echo "Usage: install_agent.sh"
 	return
 fi
-curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.3.2-amd64.deb
-dpkg -i filebeat-7.3.2-amd64.deb
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.17.23-amd64.deb
+dpkg -i filebeat-7.17.23-amd64.deb
 cp filebeat.yml /etc/filebeat
 service filebeat start
